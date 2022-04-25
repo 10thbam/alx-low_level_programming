536477571b00749d6cbffb9e00cbf633395442a1
10thbam <ameronmuluugeta@gmail.com>
2022-04-25 19:08:09 +0000

    kko

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*_memcpy - copy number adress from to address "from" to address "to"
+*@dest: content is to be copied
+*@src: source of data
+*@n: bytes of memory
+*Return: dest
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+unsigned int i;
+
+i = 0;
+while (i < n) /*Declaring WHILE*/
+{
+*(dest + i) = *(src + 1); /*add 1 positon dest and src*/
+
+} /*END WHILE*/
+
+return (dest);
+}
