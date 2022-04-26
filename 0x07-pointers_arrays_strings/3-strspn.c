43eaa03c7be0ef47001d85845cc3cb29fca94b5a
10thbam <ameronmuluugeta@gmail.com>
2022-04-26 18:38:25 +0000

    oop

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -29,10 +29,10 @@ count ++; /*count number*/
 break;
 }
 
-j++; /*add j+1*/
+j++;  /*add j+1*/
 }
 
-if (s[i] != accept[j]) /*If arent equal*/
+if (s[i] != accept[j]) /*If aren't equals*/
 {
 break;
 }
@@ -40,6 +40,5 @@ break;
 i++; /*add x+1*/
 }
 
-return (count); /* return the value of count*/
+return (count); /*return the value of count*/
 }
-
